55d4eb98ba28c646241e1ad0b42d97c0be7775fc
Brian Timba <bryantymbr@gmail.com>
2026-07-11 10:31:27 +0200

    feat: Add PostgreSQL integration with organizations model and update views

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
 import express from 'express';
+import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -31,8 +33,11 @@ app.get('/', (req, res) => {
   res.render('home', { title: 'Home' });
 });
 
-app.get('/organizations', (req, res) => {
-  res.render('organizations', { title: 'Our Partner Organizations' });
+app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();  
+    const title = 'Our Partner Organizations';
+
+    res.render('organizations', { title, organizations });
 });
 
 app.get('/projects', (req, res) => {
@@ -47,8 +52,12 @@ app.use((req, res) => {
   res.status(404).send('Page not found');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://127.0.0.1:${PORT}`);
-  console.log(`Environment: ${NODE_ENV}`);
+app.listen(PORT, async () => {
+  try {
+    await testConnection();
+    console.log(`Server is running at http://127.0.0.1:${PORT}`);
+    console.log(`Environment: ${NODE_ENV}`);
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
 });
-
